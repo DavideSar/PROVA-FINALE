66507832618d61bf0b35664a94407938b09d605b
DavideSar <davide.sargiacomo@gmail.com>
2026-02-15 19:37:58 +0100

    perfected print mean and std

--- analisi_dati.py
+++ analisi_dati.py
@@ -3,6 +3,17 @@ import pandas as pd
 import numpy as np
 import os
 
+def print_mean(sensor):
+    name = sensor["entity_id"].iloc[0]
+    mean = np.mean(sensor["_value"])
+    print(f"{name} MEAN: {mean}")
+
+
+def print_std(sensor):
+    name = sensor["entity_id"].iloc[0]
+    std = np.std(sensor["_value"])
+    print(f"{name} STD: {std}")
+
 def main():
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -34,51 +45,23 @@ def main():
     temp_cpu_lines = lines[lines['entity_id'] == 'fridge_black_internal_temperature']
     temp_probe_lines = lines[lines['entity_id'] == 'fridge_black_probe_temperature']
 
-    acc_x_mean = acc_x_lines['_value'].mean()
-    acc_y_mean = acc_y_lines['_value'].mean()
-    acc_z_mean = acc_z_lines['_value'].mean()
-    gyro_x_mean = gyro_x_lines['_value'].mean()
-    gyro_y_mean = gyro_y_lines['_value'].mean()
-    gyro_z_mean = gyro_z_lines['_value'].mean()
-    temp_mean = temp_lines['_value'].mean()
-    temp_sen55_mean = temp_sen55_lines['_value'].mean()
-    temp_cpu_mean = temp_cpu_lines['_value'].mean()
-    temp_probe_mean = temp_probe_lines['_value'].mean()
+    sensors = [
+        acc_x_lines,
+        acc_y_lines,
+        acc_z_lines,
+        gyro_x_lines,
+        gyro_y_lines,
+        gyro_z_lines,
+        temp_lines,
+        temp_sen55_lines,
+        temp_cpu_lines,
+        temp_probe_lines,
+    ]
 
-    print(f"Mean values:")
-    print(f"Acc X: {acc_x_mean}")
-    print(f"Acc Y: {acc_y_mean}")
-    print(f"Acc Z: {acc_z_mean}")
-    print(f"Gyro X: {gyro_x_mean}")
-    print(f"Gyro Y: {gyro_y_mean}")
-    print(f"Gyro Z: {gyro_z_mean}")
-    print(f"Temp: {temp_mean}")
-    print(f"Temp SEN55: {temp_sen55_mean}")
-    print(f"Temp CPU: {temp_cpu_mean}")
-    print(f"Temp Probe: {temp_probe_mean}")
-
-    acc_x_std = acc_x_lines['_value'].std()
-    acc_y_std = acc_y_lines['_value'].std()
-    acc_z_std = acc_z_lines['_value'].std()
-    gyro_x_std = gyro_x_lines['_value'].std()
-    gyro_y_std = gyro_y_lines['_value'].std()
-    gyro_z_std = gyro_z_lines['_value'].std()
-    temp_std = temp_lines['_value'].std()
-    temp_sen55_std = temp_sen55_lines['_value'].std()
-    temp_cpu_std = temp_cpu_lines['_value'].std()
-    temp_probe_std = temp_probe_lines['_value'].std()
-
-    print(f"Std values:")
-    print(f"Acc X: {acc_x_std}")
-    print(f"Acc Y: {acc_y_std}")
-    print(f"Acc Z: {acc_z_std}")
-    print(f"Gyro X: {gyro_x_std}")
-    print(f"Gyro Y: {gyro_y_std}")
-    print(f"Gyro Z: {gyro_z_std}")
-    print(f"Temp: {temp_std}")
-    print(f"Temp SEN55: {temp_sen55_std}")
-    print(f"Temp CPU: {temp_cpu_std}")
-    print(f"Temp Probe: {temp_probe_std}")
+    for sensor in sensors:
+        print_mean(sensor)
+        print_std(sensor)
+        print()
 
 if __name__ == '__main__':
     main()
