825c451e57e4af5ef12b07d79f4c0af57e08e0de
DavideSar <davide.sargiacomo@gmail.com>
2026-02-21 18:30:21 +0100

    refined useful graph, tried stepping but is not clean

--- analisi_dati.py
+++ analisi_dati.py
@@ -45,58 +45,91 @@ def main():
 
     # acc_x = remove_anomalies(lines[lines["entity_id"] == "fridge_black_accel_x"])
     acc_y = remove_anomalies(lines[lines["entity_id"] == "fridge_black_accel_y"])
+    acc_y = (
+        acc_y.groupby(np.arange(len(acc_y)) // 3)
+        .agg({"_time": "first", "_value": "mean", "entity_id": "first"})
+        .reset_index(drop=True)
+    )
     # acc_z = remove_anomalies(lines[lines["entity_id"] == "fridge_black_accel_z"])
-    gyro_x = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_x"])
-    gyro_y = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_y"])
-    gyro_z = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_z"])
+    # gyro_x = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_x"])
+    # gyro_y = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_y"])
+    # gyro_z = remove_anomalies(lines[lines["entity_id"] == "fridge_black_gyro_z"])
     temp_avg = remove_anomalies(lines[lines["entity_id"] == "airq_black_temperature"])
     # temp_sen55 = remove_anomalies(lines[lines["entity_id"] == "airq_black_temperature_sen55"])
-    temp_cpu = remove_anomalies(lines[lines["entity_id"] == "fridge_black_internal_temperature"])
+    # temp_cpu = remove_anomalies(lines[lines["entity_id"] == "fridge_black_internal_temperature"])
     temp_probe = remove_anomalies(lines[lines["entity_id"] == "fridge_black_probe_temperature"])
+    temp_probe = (
+        temp_probe.groupby(np.arange(len(temp_probe)) // 3)
+        .agg({"_time": "first", "_value": "mean", "entity_id": "first"})
+        .reset_index(drop=True)
+    )
 
-    series = [
-        (
-            "Gyroscope",
-            [
-                (gyro_x, "Gyro X", "tab:orange"),
-                (gyro_y, "Gyro Y", "tab:blue"),
-                (gyro_z, "Gyro Z", "tab:green"),
-            ],
-        ),
-        ("Temp CPU", [(temp_cpu, "Temp CPU", "tab:purple")]),
-        ("Temp Probe", [(temp_probe, "Temp Probe", "tab:brown")]),
-    ]
+    # series = [
+    #     (
+    #         "Gyroscope",
+    #         [
+    #             (gyro_x, "Gyro X", "tab:orange"),
+    #             (gyro_y, "Gyro Y", "tab:blue"),
+    #             (gyro_z, "Gyro Z", "tab:green"),
+    #         ],
+    #     ),
+    #     ("Temp Probe", [(temp_probe, "Temp Probe", "tab:brown")]),
+    # ]
 
-    fig, axes = plt.subplots(nrows=len(series) + 1, ncols=1, figsize=(14, 20), sharex=True)
+    # for ax, (group_key, group) in zip(axes, series):
+    #     for data, label, color in group:
+    #         ax.plot(data["_time"], data["_value"], linewidth=0.5, color=color, label=label)
+    #         x_min, x_max = data["_time"].min(), data["_time"].max()
+    #         ax.set_xlim(x_min, x_max)
+    #     ax.minorticks_on()
+    #     ax.grid(which='major', linestyle='-', linewidth='0.8', color='black', alpha=0.3)
+    #     ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.2)
+    #     ax.set_ylabel(group_key, fontsize=8)
+    #     if len(group) > 1:
+    #         ax.legend(fontsize=7, loc="upper right", frameon=True, framealpha=1, edgecolor='black')
 
-    for ax, (group_key, group) in zip(axes, series):
-        for data, label, color in group:
-            ax.plot(data["_time"], data["_value"], linewidth=0.5, color=color, label=label)
-        ax.minorticks_on()
-        ax.grid(which='major', linestyle='-', linewidth='0.8', color='black', alpha=0.3)
-        ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.2)
-        ax.set_ylabel(group_key, fontsize=8)
-        if len(group) > 1:
-            ax.legend(fontsize=7, loc="upper right", frameon=True, framealpha=1, edgecolor='black')
+    fig, (temp_acc_ax, probe_acc_ax) = plt.subplots(nrows=2, ncols=1, figsize=(14, 20), sharex=True)
 
-    acc_ax = axes[-1]
-    line1 = acc_ax.plot(acc_y['_time'], acc_y['_value'], linewidth=0.3, color='blue', label='Acc Y')
+    line1 = temp_acc_ax.plot(temp_avg['_time'], temp_avg['_value'], linewidth=1, color='red', label='Ext Temp')
+    temp_acc_ax.set_ylabel("Temp", fontsize=8)
+
+    acc_ax = temp_acc_ax.twinx()
+    line2 = acc_ax.plot(acc_y['_time'], acc_y['_value'], linewidth=0.2, color='blue', label='Acc Y')
     acc_ax.set_ylabel("Acc Y", fontsize=8)
 
-    temp_ax = acc_ax.twinx()
-    line2 = temp_ax.plot(temp_avg['_time'], temp_avg['_value'], linewidth=1, color='red', label='Temp')
-    temp_ax.set_ylabel("Temp", fontsize=8)
+    # Legend for acc_y and temp_avg
+    label_lines = line1 + line2
+    labels = [line.get_label() for line in label_lines]
+    temp_acc_ax.legend(label_lines, labels, fontsize=10, loc="upper right", frameon=True, framealpha=1, edgecolor='black')
 
-    # temp_prob_ax = acc_ax.twinx()
-    # temp_prob_ax.plot(temp_probe['_time'], temp_probe['_value'], linewidth=1, color='purple', label='Temp')
-    # temp_prob_ax.set_ylabel("Temp", fontsize=8)
+    # Set grid for temp_acc_ax
+    temp_acc_ax.minorticks_on()
+    temp_acc_ax.grid(which='major', linestyle='-', linewidth='0.8', color='black', alpha=0.3)
+    temp_acc_ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.2)
 
-    acc_ax.minorticks_on()
-    acc_ax.grid(which='major', linestyle='-', linewidth='0.8', color='black', alpha=0.3)
-    acc_ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.2)
+    line3 = probe_acc_ax.plot(temp_probe['_time'], temp_probe['_value'], linewidth=1, color='green', label='Fridge Temp')
+    probe_acc_ax.set_ylabel("Temp", fontsize=8)
+
+    acc_ax = probe_acc_ax.twinx()
+    line4 = acc_ax.plot(acc_y['_time'], acc_y['_value'], linewidth=0.2, color='blue', label='Acc Y')
+    acc_ax.set_ylabel("Acc Y", fontsize=8)
+
+    # Legend for acc_y and probe_temp
+    label_lines = line3 + line4
+    labels = [line.get_label() for line in label_lines]
+    probe_acc_ax.legend(label_lines, labels, fontsize=10, loc="upper right", frameon=True, framealpha=1, edgecolor='black')
+
+    # Set grid for probe_acc_ax
+    probe_acc_ax.minorticks_on()
+    probe_acc_ax.grid(which='major', linestyle='-', linewidth='0.8', color='black', alpha=0.3)
+    probe_acc_ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.2)
+
+    # Set x-axis limits
+    all_times = pd.concat([acc_y, temp_avg, temp_probe])['_time'].sort_values().unique()
+    temp_acc_ax.set_xlim(all_times.min(), all_times.max())
 
     fig.suptitle("Sensors Data - 25/07/2022", fontsize=14, fontweight="bold", y=1)
-    axes[-1].set_xlabel("Time")
+    probe_acc_ax.set_xlabel("Time")
     plt.tight_layout()
     plt.show()
 
